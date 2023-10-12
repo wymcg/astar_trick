f264d5434ad2da606a0056941a2ee2e45c10017e
Will McGloughlin <willem.mcg@gmail.com>
2023-10-11 20:15:53 -0500

    Update plugin for Matricks v0.3.0

--- src/plugin_fns.rs
+++ src/plugin_fns.rs
@@ -3,8 +3,6 @@ use std::ops::DerefMut;
 
 use extism_pdk::*;
 use lazy_static::lazy_static;
-use matricks_plugin::{MatrixConfiguration, PluginUpdate};
-use serde_json::from_str;
 use std::sync::{Arc, Mutex};
 use crate::util::euclidian_distance;
 
@@ -21,24 +19,21 @@ const PATH_COLOR: [u8; 4] = [0, 64, 0, 64];
 const DISTANCE_SCALE: f32 = 10_000.0;
 
 lazy_static! {
-    static ref CONFIG: Arc::<Mutex::<MatrixConfiguration>> =
-        Arc::new(Mutex::new(MatrixConfiguration::default()));
     static ref STATE: Arc::<Mutex::<PluginState>> = Arc::new(Mutex::new(PluginState::default()));
 }
 
 #[plugin_fn]
-pub fn setup(cfg_json: String) -> FnResult<()> {
-    // Save the matrix config
-    let mut config = CONFIG.lock().unwrap();
-    let config = config.deref_mut();
-    *config = from_str(&*cfg_json).expect("Unable to deserialize matrix config!");
+pub fn setup(_: ()) -> FnResult<()> {
+    // Get the width and height from the config
+    let width: usize = config::get("width").unwrap().parse().unwrap();
+    let height: usize = config::get("height").unwrap().parse().unwrap();
 
-    // Generate the matrix
+    // Generate the maze
     let mut state = STATE.lock().unwrap();
     let state = state.deref_mut();
-    state.generate_maze(config.width, config.height, None);
+    state.generate_maze(width, height, None);
 
-    // Start the BFS
+    // Set up the A* algorithm
     state.queue.push(state.maze.start, ((1.0 / euclidian_distance(state.maze.start, state.maze.goal)) * DISTANCE_SCALE) as u32);
     state.previous.insert(state.maze.start, None);
 
@@ -46,38 +41,41 @@ pub fn setup(cfg_json: String) -> FnResult<()> {
 }
 
 #[plugin_fn]
-pub fn update(_: ()) -> FnResult<Json<PluginUpdate>> {
-    // Grab our matrix config and plugin state
-    let config = CONFIG.lock().unwrap();
+pub fn update(_: ()) -> FnResult<Json<Option<Vec<Vec<[u8;4]>>>>> {
+    // Get the width and height from the config
+    let width: usize = config::get("width").unwrap().parse().unwrap();
+    let height: usize = config::get("height").unwrap().parse().unwrap();
+
+    // Grab our  plugin state
     let mut state = STATE.lock().unwrap();
     let state = state.deref_mut();
 
-    // Make a blank matrix
-    let mut matrix_state: Vec<Vec<[u8; 4]>> = vec![vec![NONE_COLOR; config.width]; config.height];
+    // Make a colorless matrix
+    let mut matrix_state: Vec<Vec<[u8; 4]>> = vec![vec![NONE_COLOR; width]; height];
 
-    // Add the walls
+    // Draw the walls
     for (x, y) in &state.maze.wall_coords {
         matrix_state[*y][*x] = WALL_COLOR;
     }
 
-    // Add visited coordinates
+    // Draw visited sections
     for (x, y) in &state.visited {
         matrix_state[*y][*x] = VISITED_COLOR;
     }
 
-    // Add enqueued coordinates
+    // Draw enqueued sections
     for ((x, y), _p) in &state.queue {
         matrix_state[*y][*x] = ENQUEUED_COLOR;
     }
 
-    // Add the start and the goal
+    // Draw the start and the goal
     matrix_state[state.maze.start.1][state.maze.start.0] = START_COLOR;
     matrix_state[state.maze.goal.1][state.maze.goal.0] = GOAL_COLOR;
 
     if !state.visited.contains(&state.maze.goal) {
         // If the goal has not been found, do a step of the BFS
 
-        // Pull a next coordinate from the queue
+        // Pull the next coordinate from the queue
         match state.queue.pop() {
             None => {}
             Some(c) => {
@@ -95,9 +93,9 @@ pub fn update(_: ()) -> FnResult<Json<PluginUpdate>> {
                 ];
                 let unvisited_neighbors = candidates.iter()
                     .filter(|c| 0 <= c.0)
-                    .filter(|c| c.0 < config.width as i32)
+                    .filter(|c| c.0 < width as i32)
                     .filter(|c| 0 <= c.1)
-                    .filter(|c| c.1 < config.height as i32)
+                    .filter(|c| c.1 < height as i32)
                     .map(|c| (c.0 as usize, c.1 as usize))
                     .filter(|c| !state.maze.wall_coords.contains(c))
                     .filter(|c| !state.visited.contains(c));
@@ -125,10 +123,5 @@ pub fn update(_: ()) -> FnResult<Json<PluginUpdate>> {
         }
     }
 
-    Ok(Json(PluginUpdate {
-        state: matrix_state,
-        done: false,
-        log_message: None,
-        ..Default::default()
-    }))
+    Ok(Json(Some(matrix_state)))
 }
